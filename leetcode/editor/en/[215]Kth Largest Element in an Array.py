8bbf7691f4ea6a998f74a40bc8de5b92ad57e530
baichenxu <bai_chauncey@foxmail.com>
2022-05-16 11:24:35 -0700

    215的最大堆解法

--- leetcode/editor/en/[215]Kth Largest Element in an Array.py
+++ leetcode/editor/en/[215]Kth Largest Element in an Array.py
@@ -32,41 +32,40 @@ class Solution:
         n = len(nums)
         # 构建最大堆
         self.buildMaxHeap(nums)
-        # 扔k-1个最大数，下沉k-1次
+        # 构建完后 pop k-1 次
         for i in range(k - 1):
             nums[0], nums[n - 1 - i] = nums[n - 1 - i], nums[0]
             self.adjustDown(nums, 0, n - 1 - i - 1)
         return nums[0]
 
+    # 构建最大堆
     def buildMaxHeap(self, nums: List[int]) -> None:
         n = len(nums)
-        # 对前一半进行下沉操作 从n//2到0
+        # 对前一半进行下沉 从n//2到0（从后往前，对非叶子节点进行下沉）
         for root in range(n // 2, -1, -1):
             self.adjustDown(nums, root, n - 1)
-            print(nums)
 
-    #
+    # 对root进行下沉操作
     def adjustDown(self, nums: List[int], root: int, hi: int) -> None:
         if root > hi:
             return
         t = nums[root]
-        child = 2 * root + 1  # 左子
-        # 当还有孩子的时候
+        child = 2 * root + 1  # root的左子
+        # root为非叶子节点时（叶子节点无法下沉）
         while child <= hi:
-            # 如果右子没出界，并且左子小于右子
+            # 选它比较大的那个孩子
             if child + 1 <= hi and nums[child] < nums[child + 1]:
-                # 处理右子
                 child += 1
-            # 如果初始根不比当前的子小,跳出循环，当前的根等于初始根
+            # 如果要下沉的元素比最大的子不小，跳出循环，当前的根等于要下沉的元素（下沉到位置了）
             if t >= nums[child]:
                 break
-            # 如果根比子小，把子的值赋予根，
+            # 如果根比子小，把子的值赋予根（子上浮）
             nums[root] = nums[child]
             # 根指向子
             root = child
             # 子指向新的根的子
             child = 2 * root + 1
-        # 叶子节点赋为最开始的root的值
+        # 叶子节点赋为最开始的root的值（子上浮结束，下沉元素的位置为root）
         nums[root] = t
 
 
@@ -75,4 +74,4 @@ class Solution:
 
 if __name__ == '__main__':
     a = Solution()
-    a.findKthLargest([123, 2, 3, 4,5,6,7,8], 4)
+    a.findKthLargest([1, 2, 3, 4, 5, 6, 7, 8], 8)
