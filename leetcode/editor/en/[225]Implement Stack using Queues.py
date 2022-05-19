3f87d00c181a83428bcd2ef87dc85db3c84e7595
baichenxu <bai_chauncey@foxmail.com>
2022-05-19 10:15:21 -0700

    昨天写完的

--- leetcode/editor/en/[225]Implement Stack using Queues.py
+++ leetcode/editor/en/[225]Implement Stack using Queues.py
@@ -52,7 +52,7 @@
 #  
 #  Follow-up: Can you implement the stack using only one queue? 
 #  Related Topics Stack Design Queue 👍 2831 👎 870
-
+import collections
 from typing import List, Optional
 from dataStructure.ListNode import ListNode
 
@@ -61,19 +61,23 @@ from dataStructure.ListNode import ListNode
 class MyStack:
 
     def __init__(self):
-        
+        self.queue1 = collections.deque()
+        self.queue2 = collections.deque()
 
     def push(self, x: int) -> None:
-        
+        self.queue2.append(x)
+        while self.queue1:
+            self.queue2.append(self.queue1.popleft())
+        self.queue1, self.queue2 = self.queue2, self.queue1
 
     def pop(self) -> int:
-        
+        return self.queue1.popleft()
 
     def top(self) -> int:
-        
+        return self.queue1[0]
 
     def empty(self) -> bool:
-        
+        return not self.queue1
 
 
 # Your MyStack object will be instantiated and called as such:
